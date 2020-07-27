bce4ac3906079406cf7ee6b78028342627d6ccb9
ZW00HD <1027069573@qq.com>
2020-07-27 16:46:15 +0800

    新增

---
+++ HelloWord/src/com/imti/hello/Frist.java
@@ -0,0 +1,18 @@
+package com.imti.hello;
+/**@文件名: Frist.java
+ * @类功能说明: 
+ * @作者: ZhangWen
+ * @Email: 1027069573@qq.com
+ * @日期: 2020年7月27日下午2:26:13
+ * @修改说明:<br> 
+ * <pre>
+ * 	 <li>作者: ZhangWen</li> 
+ * 	 <li>日期: 2020年7月27日下午2:26:13</li> 
+ *	 <li>内容: </li>
+ * </pre>
+ */
+public class Frist {
+	public static void main(String[] args) {
+		System.out.println("今天2020年7月27日，星期一，今天学习Svn和Git");
+	}
+}
